77105c770a31cc825055a1813cdab6af763f6172
Elim Florvil <elim.florvil@ceo-vision.com>
2023-11-27 14:57:56 +0100

    Add validation pipe and Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,40 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Metatube API')
+    .setDescription('Api of the Metatube application ')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .setLicense('MIT License', 'https://opensource.org/licenses/MIT')
+    .build();
 
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  // Define custom Swagger UI configuration
+  const swaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+    customSiteTitle: 'Api Metatube Production',
+    customfavIcon:
+      'https://static.vecteezy.com/system/resources/previews/018/930/572/original/youtube-logo-youtube-icon-transparent-free-png.png',
+  };
+
+  SwaggerModule.setup('', app, document, swaggerCustomOptions);
+  await app.listen(process.env.PORT || 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+// eslint-disable-next-line prettier/prettier
 bootstrap();
