4b6b307afc73fc101b2b0fe6904002a8fa32a3e5
Elim Florvil <elim.florvil@ceo-vision.com>
2023-11-27 15:19:39 +0100

    Add dependencies and configuration for NestJS Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+    envFilePath: '.env', // Chemin vers le fichier .env
+    isGlobal: true,
+  }),
+  MongooseModule.forRoot(process.env.DB_URI),
+  AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService, UserService],
 })
