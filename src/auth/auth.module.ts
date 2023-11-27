941f25a079455afd52bb563b7511242c28ea0579
Elim Florvil <elim.florvil@ceo-vision.com>
2023-11-27 14:57:49 +0100

    Add AuthModule and UserModule to AppModule

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User, UserSchema } from './entities/user.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy],
+})
+export class AuthModule {}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
+//import * as bcrypt from 'bcrypt';
+@Injectable()
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async validateUser(username: string, password: string): Promise<User> {
+    const user = await this.userModel.findOne({ username }).exec();
+
+    if (user && (await user.validatePassword(password))) {
+      return user;
+    }
+    throw new UnauthorizedException('Invalid credentials');
+  }
+}
