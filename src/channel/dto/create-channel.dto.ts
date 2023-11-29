601fd022d5356f158e828a4b8cd3d7b1d8304975
Elim Florvil <elim.florvil@icloud.com>
2023-11-29 21:14:31 +0100

    Refactor CreateChannelDto to use channelName instead of username, email, and password

--- src/channel/dto/create-channel.dto.ts
+++ src/channel/dto/create-channel.dto.ts
@@ -1,21 +1,7 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 export class CreateChannelDto {
   @IsString()
-  @ApiProperty({ example: 'John Doe ', description: 'The name of your user' })
-  username: string;
-
-  @IsEmail()
-  @ApiProperty({
-    example: 'johndoe@example45.com ',
-    description: 'The email of your user',
-  })
-  email: string;
-
-  @IsString()
-  @ApiProperty({
-    example: 'password ',
-    description: 'The password of your user',
-  })
-  password: string;
+  @ApiProperty({ example: 'MrBeast', description: 'The name of your channel' })
+  channelName: string;
 }
